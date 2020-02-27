9c82c06a95c47e649d9ea0866c9afd5ff5daf11d
mateusfg7 <mateusfelipefg77@gmail.com>
2020-02-27 18:05:18 -0300

    use 'clear' function

--- Twitter.py
+++ Twitter.py
@@ -1,8 +1,10 @@
+from functions.text import clear
 from functions.text import banner
 from functions.text import choice_menu
 
 from choices.choice import choice_request
 
+clear()
 print(banner())
 
 while True:
--- choices/choice.py
+++ choices/choice.py
@@ -1,6 +1,9 @@
 from pprint import pprint
 
 from functions.response import handling_response
+
+from functions.text import clear
+from functions.text import banner
 from functions.text import update_status_response_text
 
 from api_requests.api_request import status_update
@@ -8,8 +11,11 @@ from api_requests.api_request import status_update
 def choice_request(choice):
     
     if choice == '1':
+
+        clear()
+        print(banner())
         
-        tweet = input('text: ')
+        tweet = input('\nTweet: ')
         
         updateStatusRequest = status_update(tweet)
         requestObject = handling_response(updateStatusRequest[1])
