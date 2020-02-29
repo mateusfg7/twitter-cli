6d6a7d5d18207711e6cc47e5ca7224d3757afa98
mateusfg7 <mateusfelipefg77@gmail.com>
2020-02-29 08:35:42 -0300

    use choices method

--- choices/choice_switch.py
+++ choices/choice_switch.py
@@ -1,37 +1,12 @@
-from pprint import pprint
-
-from functions.response import handling_response
-
-from api_requests.api_request import status_update
-from api_requests.api_request import home_timeline
-
-from functions.text import clear
-from functions.text import banner
-from functions.text import update_status_response_text
-from functions.text import home_timelin_response_text
+from choices.choice_method import status_update_function
+from choices.choice_method import home_timeline_function
 
 def choice_switch(choice):
-    
     if choice == '1':
-        clear()
-        print(banner())
-        
-        tweet = input('\nTweet: ')
-        
-        updateStatusRequest = status_update(tweet)
-        responseObject = handling_response(updateStatusRequest[1])
-        
-        print(update_status_response_text(responseObject))
+        status_update_function()
     
     if choice == '2':
-        clear()
-        print(banner())
-
-        homeTimelineRequest = home_timeline()
-        responseObject = handling_response(homeTimelineRequest[1])
-
-        print(home_timelin_response_text(responseObject[1]))
-        # pprint(responseObject[1])
+        home_timeline_function()
     
     elif choice == '0':
         print('exiting...')
