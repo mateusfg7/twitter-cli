2c46841eae8e906d80874e5615e9ab184e9479c4
mateusfg7 <mateusfelipefg77@gmail.com>
2020-02-29 08:32:19 -0300

    linked modifyed name file

--- Twitter.py
+++ Twitter.py
@@ -2,7 +2,7 @@ from functions.text import clear
 from functions.text import banner
 from functions.text import choice_menu
 
-from choices.choice import choice_request
+from choices.choice_switch import choice_switch
 
 clear()
 print(banner())
@@ -10,4 +10,4 @@ print(banner())
 while True:
     print(choice_menu())
     query = input('> ')
-    choice_request(query)
+    choice_switch(query)
